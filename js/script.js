43f556fabe51b6a1eaf68737b02c3602be9d8e84
maicol <maicol.matteoli@gmail.com>
2021-06-21 15:58:31 +0200

    add : h1 + img

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var app = new Vue(
+    {
+        el: "#app",
+
+        data:{
+            messaggio : "hellooooo ",
+            immagine : 'img/arch-bridge.jpg',
+            // item : <div>sono un div</div> , 
+        },
+
+        methods:{
+
+        }
+    }
+);
